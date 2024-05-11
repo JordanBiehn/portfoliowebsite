22eaa014f15ffc0efe7518f090fb44981753cd39
JordanBiehn <163483395+JordanBiehn@users.noreply.github.com>
2024-05-11 14:13:08 -0500

    Update scripts.js
    Updating nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-<script>
-  alert("Hello");
-</script>
 console.log("Hello world");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
